367e8acda44885ddbc11e2acf00c005d23c62941
Volodymyr Manko <vmanko1@gmail.com>
2016-10-29 12:01:15 +0300

    Changed auth.js

--- auth.js
+++ auth.js
@@ -1,7 +1,3 @@
 function authenticate (login, password) {
-  if (login === 'login' && password === 'password') {
-    return 'You were logged in';
-  } else {
-    return 'Login is incorrect';
-  }
+  return 'Login is not corrent';
 }
