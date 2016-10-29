25bc0b4ee57cec0ce6fe5ef35918f63a1343c810
Volodymyr Manko <vmanko1@gmail.com>
2016-10-29 11:59:18 +0300

    Changed auth on conflicted-branch

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 function authenticate (login, password) {
-  if (login === 'login' && password === 'password') {
-    return 'You were logged in';
+  if (login === 'login' || password === 'password') {
+    return 'You are logged in';
   } else {
     return 'Login is incorrect';
   }
